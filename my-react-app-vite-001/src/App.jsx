b3ed8f014d3b5e533a34f43e0a07978dfca87685
Piotr <piotrdusinski01@gmail.com>
2023-11-27 21:49:36 +0100

    Main work on!

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Starter from './tutorial/01-useState/starter/01-error-example';
+import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <h1>Pierwszy projekt Vite</h1>
+  return (
+    <div className='container'>
+      <Starter/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ my-react-app-vite-001/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -0,0 +1,18 @@
+const ErrorExample = () => {
+  let number = 0;
+
+  const increaseNumber = () =>
+  {
+    number = number + 1;
+  };
+
+  return (
+    <div>
+      <h2>useState error example</h2>
+      <button type="button" onClick={increaseNumber}className="btn">Increase the number by one</button>
+      <h2>Number: {number}</h2>
+    </div>
+  );
+};
+
+export default ErrorExample;
