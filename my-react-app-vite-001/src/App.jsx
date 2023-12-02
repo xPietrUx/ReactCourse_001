8b017f866bda300071d62d45407f33517f7ceffa
Piotr <piotrdusinski01@gmail.com>
2023-12-02 22:27:32 +0100

    useState working with array

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/01-useState/starter/02-useState-basics';
+import Starter from './tutorial/01-useState/starter/03-useState-array';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- my-react-app-vite-001/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ my-react-app-vite-001/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+
+  const[Tablica, setTablica] = useState(data);
+
+  const removeItem = (id) => {
+    //console.log(id);
+    const newTablica = Tablica.filter((pearson) => pearson.id !== id)
+    setTablica(newTablica); 
+  };
+  const removeAllItems = () => {
+    setTablica([]);
+  };
+
+  return( 
+  <div>
+    <h2>useState array example</h2>
+    <ul>
+      {Tablica.map((person) => {
+        const {id, name} = person;
+        //console.log(person)
+        return(
+          <div key={id}>
+          <h4>{name}</h4>
+          <button type="button" className="btn" onClick={() => removeItem(id)}>Remove</button>
+          </div>
+        );
+      })}
+    </ul>
+    <button type="button" className="btn" onClick={removeAllItems}>Remove all</button>
+  </div>
+  );
 };
 
 export default UseStateArray;
+
