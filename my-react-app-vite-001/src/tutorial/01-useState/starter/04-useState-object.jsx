be7a580acf570be60699199e275eaa30244f9b5d
Piotr <piotrdusinski01@gmail.com>
2023-12-04 14:14:51 +0100

    Displaying elements one by one by using button

--- my-react-app-vite-001/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ my-react-app-vite-001/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,30 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const[counter, setCounter] = useState(0); 
+
+  const people = [
+    {id: 0, name: 'Peter', age: 24, hobby: 'Read books'},
+    {id: 1, name: 'John', age: 28, hobby: 'Screams At The Computer'},
+  ];
+
+  const increaseCounter = () =>
+  {
+    setCounter(counter + 1);
+  }
+
+  const elementToFind = people.find(element => element.id === counter);
+
+  return (
+    <ul>
+       <li>{elementToFind?.name}</li> 
+       <li>{elementToFind?.age}</li>
+       <li>{elementToFind?.hobby}</li>
+       <button className='btn' onClick={increaseCounter}>Next person</button>
+    </ul>
+  );
+
 };
 
 export default UseStateObject;
