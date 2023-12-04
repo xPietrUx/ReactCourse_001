b94b56b8f2dd7f72ebd18068e5ea9cf3deb8cd1e
Piotr <piotrdusinski01@gmail.com>
2023-12-04 14:15:06 +0100

    04.12.2023

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,10 +1,11 @@
-import Starter from './tutorial/01-useState/starter/03-useState-array';
+import Starter from './tutorial/01-useState/starter/04-useState-object';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
   return (
     <div className='container'>
       <Starter/>
+      {/* <Final/> */}
     </div>
   );
 }
