c03383f63072788f7e6d886873c9698510d9b881
Piotr <piotrdusinski01@gmail.com>
2023-12-10 17:29:09 +0100

    useState-object what it should look like

--- my-react-app-vite-001/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ my-react-app-vite-001/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 const UseStateObject = () => {
 
+/*
   const[counter, setCounter] = useState(0); 
-
+ 
   const people = [
     {id: 0, name: 'Peter', age: 24, hobby: 'Read books'},
     {id: 1, name: 'John', age: 28, hobby: 'Screams At The Computer'},
@@ -23,8 +24,26 @@ const UseStateObject = () => {
        <li>{elementToFind?.hobby}</li>
        <button className='btn' onClick={increaseCounter}>Next person</button>
     </ul>
-  );
+  ); 
+*/
 
+const [name, setName] = useState('Peter');
+const [age, setAge] = useState(24);
+const [hobby, setHobby] = useState('Read books');
+
+const displayPerson = () => {
+  setName('John');
+  setAge(28);
+  setHobby('Scream at the computer');
+}
+
+return(
+<>
+  <h3>{name}</h3>
+  <h3>{age}</h3>
+  <h3>{hobby}</h3>
+  <button className='btn' onClick={displayPerson}>Show John</button>
+</>)
 };
 
 export default UseStateObject;
