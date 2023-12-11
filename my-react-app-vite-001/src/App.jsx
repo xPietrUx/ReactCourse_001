6909c6e0c9d69b208eb78b6dee5720f1a608790c
Piotr <piotrdusinski01@gmail.com>
2023-12-11 11:56:27 +0100

    useState-gotcha, proper counting

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/01-useState/starter/04-useState-object';
+import Starter from './tutorial/01-useState/starter/05-useState-gotcha';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- my-react-app-vite-001/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ my-react-app-vite-001/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+
+  const [gotcha, setGotcha] = useState(0);
+
+  const setFunction = () =>{
+    setGotcha((currentState)=>{
+      const newState = currentState + 1;
+      return newState;
+    });  
+    console.log(gotcha);
+  }
+
+  return(
+  <>
+    <h4>Show number {gotcha}</h4>
+    <button className="btn" onClick={setFunction}>Increase by one</button>
+  </>
+  );
 };
 
 export default UseStateGotcha;
