6510728082a2b42876fa9fa62ddd04f02166d66b
Piotr <piotrdusinski01@gmail.com>
2023-12-11 19:37:40 +0100

    multiple returns + if's

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial//03-conditional-rendering/starter/01-multiple-returns-basics';
+import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,46 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+
+  const[isLoading, setIsLoading] = useState(true);
+  const[isError, setIsError] = useState(false);
+
+  const [users, setUsers] = useState(null);
+
+  useEffect(()=>{
+    const fetchData = async () => 
+    { 
+      try 
+      {
+        const response = await fetch(url);
+        const users = await response.json();
+        setUsers(users);
+      } 
+      catch (error) 
+      {
+        setIsError(true);
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  if(isLoading)
+  {
+    return <h2>Loading...</h2>
+  }
+  if(isError)
+  {
+    return <h2>Error!</h2>
+  }
+
+  return (
+    <div>
+      <img src={users.avatar_url} alt={users.name} />
+      <h2>{users.name}</h2>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
