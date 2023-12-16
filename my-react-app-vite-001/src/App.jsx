41ac3beabf2261bebb219ad020fbba34137c34c4
Piotr <piotrdusinski01@gmail.com>
2023-12-16 22:06:52 +0100

    Complet 03-conditions

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/06-toggle-challenge';
+import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    // normally connect to db or api
+    setUser({ name: 'vegan food truck' });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>hello there, {user.name}</h4>
+          <button className='btn' onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button className='btn' onClick={login}>
+            login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
