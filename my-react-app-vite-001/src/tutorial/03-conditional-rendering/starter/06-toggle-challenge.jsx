98cc36376fb90d92d871eec70997423970fd1003
Piotr <piotrdusinski01@gmail.com>
2023-12-16 21:31:52 +0100

    Toggle challange complete

--- my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+
+  const [decision, setDecision] = useState(false);
+  
+  const ifDecision = () => 
+  { 
+    if(decision == false)
+    {
+      setDecision(true);
+    }
+    else
+    {
+      setDecision(false);
+    }
+  }
+
+  return (
+    <>
+      <h2>Show me, what you got!</h2>
+      <button className="btn" 
+      onClick={ifDecision}>Show</button>
+      {decision && (
+        <h1>Suprise!🎉🎈🎊</h1>
+      )}
+  
+      {/* 
+      How to do it in master's way
+      <button className='btn' onClick={() => setShowAlert(!showAlert)}></button> 
+      */}
+    </>
+  );
 };
 
 export default ToggleChallenge;
