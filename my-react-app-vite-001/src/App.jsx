9b2b8ac4eae25624faa34e1cae067af3f88f5187
Piotr <piotrdusinski01@gmail.com>
2023-12-16 18:13:01 +0100

    How falsy and truthy works in states

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview';
 import Final from './tutorial/01-useState/final/01-error-example';
 
 function App() {
--- my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ my-react-app-vite-001/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,21 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+
+  // Falsy
+  const [text, setText] = useState('');
+
+  // Truthy
+  const [name, setName] = useState('susan');
+
+  const codeExample = text || 'hello world';
+
+  return <div>
+    <h4>Falsy OR: {text || 'hello world'}</h4>
+    <h4>Falsy AND: {text && 'hello world'}</h4>
+    <h4>Truthy OR: {name || 'hello world'}</h4>
+    <h4>Truthy AND: {name && 'hello world'}</h4>
+    {codeExample}
+  </div>;
 };
 export default ShortCircuitOverview;
