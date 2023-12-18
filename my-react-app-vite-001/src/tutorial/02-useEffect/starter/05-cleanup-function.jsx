8d3220e54dd8ef272cfbbc1e955cf5b2b7b8c960
Piotr <piotrdusinski01@gmail.com>
2023-12-18 12:06:00 +0100

    Cleanup, when to use it

--- my-react-app-vite-001/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ my-react-app-vite-001/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+
+  const [value, setValue] = useState(false);
+
+  console.log('render');
+
+  return (
+    <>
+      <button className="btn" onClick={ () => {setValue(!value)}  }>Toggle</button>
+
+      { value && <RandomComponent/> }
+    </>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(  () => {
+
+    /* const intId = setInterval(  () => {
+      console.log('hello from interval');
+    }, 1000  );
+
+    return () => {
+      clearInterval(intId);
+      console.log('cleanup');
+    }; */
+
+    const someFunc = () => {
+
+    };
+
+    window.addEventListener('scroll', someFunc);
+    return () => window.removeEventListener('scroll', someFunc);
+
+  }, []);
+
+  return <h1>Something</h1>;
 };
 
 export default CleanupFunction;
