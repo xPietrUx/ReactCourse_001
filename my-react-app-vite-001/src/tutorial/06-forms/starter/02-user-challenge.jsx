b99313712aae4a232ee5348dee6e5811bd8ad405
Piotr <piotrdusinski01@gmail.com>
2023-12-26 17:26:26 +0100

    How you can work with arrays by submit

--- my-react-app-vite-001/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ my-react-app-vite-001/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,63 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UserChallenge = () => {
+
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState(data);
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name)
+      return;
+    const fakeId = Date.now();
+    const newUser = { id: fakeId, name: name };
+    const updatedUsers = [...users, newUser];
+    setUsers(updatedUsers);
+    setName('');
+  };
+
+  const removeUser = (id) => {
+    const updatedUsers = users.filter( (person) => person.id !== id );
+    setUsers(updatedUsers);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input 
+          type='text' 
+          className='form-input' 
+          id='name' 
+          name="name"
+          onChange={handleChange}/>
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button 
+        type='submit' 
+        className='btn btn-block'>
           submit
         </button>
       </form>
       {/* render users below */}
+      <h2>Users:</h2>
+      { users.map((user) => (
+          <div>
+            <h4>{user.name}</h4>
+            <button className="btn"
+            onClick={ () => removeUser(user.id) }
+            >Remove</button>
+          </div>
+      ))}
     </div>
   );
 };
