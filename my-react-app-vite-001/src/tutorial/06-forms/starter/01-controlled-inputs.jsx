b9e441cd067fcb116b9dd63fd91b1670b2755158
Piotr <piotrdusinski01@gmail.com>
2023-12-26 16:44:27 +0100

    Form setup

--- my-react-app-vite-001/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ my-react-app-vite-001/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleChange = (e) => {
+    // console.log(e.targe.name);
+    // console.log(e.targe.value);
+    setName(e.targe.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // do sth
+    console.log(name, email);
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>Controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">Name</label>
+        <input 
+        type="text" 
+        id="name" 
+        className="form-input" 
+        value={name} 
+        onChange={handleChange}/>
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">Email</label>
+        <input 
+        type="email" 
+        id="name" 
+        className="form-input"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
+      </div>
+      <button type="submit" className="btn btn-block">Submit</button>
+    </form>
+  );
 };
 export default ControlledInputs;
