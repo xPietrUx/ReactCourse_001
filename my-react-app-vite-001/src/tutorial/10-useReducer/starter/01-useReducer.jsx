8216477cac89a1657c16d07a51653bc7a5b99a94
Piotr Dusinski <piotrdusinski01@gmail.com>
2024-01-02 21:38:05 +0100

    How useReducer work's

--- my-react-app-vite-001/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ my-react-app-vite-001/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
-import { data } from '../../../data';
-import { useState, useReducer } from 'react';
+import React from "react";
+import { data } from "../../../data";
+import { useState, useReducer } from "react";
 
 const defaultState = {
   people: data,
-  isLoading: false
+  isLoading: false,
 };
 
-const CLEAR_LIST = 'CLEAR_LIST';
-const RESET_LIST = 'RESET_LIST';
-const REMOVE_ITEM = 'REMOVE_ITEM';
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
 
 const reducer = (state, action) => {
-  if(action.type === 'CLEAR_LIST'){
-    return {...state, people: []};
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] };
   }
+
+  if (action.type === RESET_LIST) {
+    return { ...state, people: data };
+  }
+
+  if (action.type === REMOVE_ITEM) {
+    let newPeople = state.people.filter(
+      (person) => person.id !== action.payload.id,
+    );
+    return { ...state, people: newPeople };
+  }
+
+  throw new Error(`No matching "${action.type}" - action type`);
 };
 
 const ReducerBasics = () => {
-
   const [state, dispatch] = useReducer(reducer, defaultState);
 
-  const [people, setPeople] = useState(data);
-
   const removeItem = (id) => {
-    // let newPeople = people.filter((person) => person.id !== id);
-    // setPeople(newPeople);
+    dispatch({ type: REMOVE_ITEM, payload: { id: id } });
   };
 
   const clearList = () => {
-    dispatch({type: 'CLEAR_LIST'});
-
-    // setPeople([]);
+    dispatch({ type: CLEAR_LIST });
   };
 
   const resetList = () => {
-    // setPeople(data);
+    dispatch({ type: RESET_LIST });
   };
 
   return (
@@ -43,7 +50,7 @@ const ReducerBasics = () => {
       {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
@@ -51,25 +58,21 @@ const ReducerBasics = () => {
       })}
 
       {state.people.length > 0 ? (
-
         <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}
         >
           clear items
         </button>
-
       ) : (
-
         <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={resetList}
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetList}
         >
           Reset
         </button>
-
       )}
     </div>
   );
