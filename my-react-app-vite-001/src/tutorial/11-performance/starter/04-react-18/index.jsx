58127c4d26a47efdfc6f7c039dced52f925a2602
Piotr <piotrdusinski01@gmail.com>
2024-01-06 15:46:43 +0100

    useTransition to not blocking whole UI

--- my-react-app-vite-001/src/tutorial/11-performance/starter/04-react-18/index.jsx
+++ my-react-app-vite-001/src/tutorial/11-performance/starter/04-react-18/index.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 const LatestReact = () => {
   const [text, setText] = useState('');
   const [items, setItems] = useState([]);
+  const [isPending, startTransition] = useTransition();
 
   const handleChange = (e) => {
     setText(e.target.value);
 
     // slow down CPU
-    // const newItems = Array.from({ length: 5000 }, (_, index) => {
-    //   return (
-    //     <div key={index}>
-    //       <img src='/vite.svg' alt='' />
-    //     </div>
-    //   );
-    // });
-    // setItems(newItems);
+    startTransition(() => {
+      const newItems = Array.from({ length: 5000 }, (_, index) => {
+        return (
+          <div key={index}>
+            <img src='/vite.svg' alt='' />
+          </div>
+        );
+      });
+      setItems(newItems);
+    });
   };
+
   return (
     <section>
       <form className='form'>
@@ -28,15 +32,19 @@ const LatestReact = () => {
       </form>
       <h4>Items Below</h4>
 
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr 1fr',
-          marginTop: '2rem',
-        }}
-      >
-        {items}
-      </div>
+      {isPending ? (
+        <h4>Loading...</h4>
+      ) : (
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            marginTop: '2rem',
+          }}
+        >
+          {items}
+        </div>
+      )}
     </section>
   );
 };
