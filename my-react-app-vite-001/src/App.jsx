ce96bfd7c6002efb7ae4777bfd30bf26d4fe37a8
Piotr Dusinski <piotrdusinski01@gmail.com>
2024-01-06 14:50:57 +0100

    useMemo() is used to don't block whole UI

--- my-react-app-vite-001/src/App.jsx
+++ my-react-app-vite-001/src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/04-fetch-data';
+import Starter from './tutorial/11-performance/starter/04-react-18';
 
 function App() {
   return <Starter />;
--- my-react-app-vite-001/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ my-react-app-vite-001/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,9 +1,15 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import { data } from '../../../../data';
 import List from './List';
+import slowFunction from './slowFunction';
+
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
+
+  const value = useMemo(() => slowFunction(), []);
+  slowFunction();
+  console.log(value);
 
   const removePerson = useCallback(
     (id) => {
