5cf03a4a92b51171a27b92a148326a0281acb54b
Piotr <piotrdusinski01@gmail.com>
2024-01-14 11:45:27 +0100

    increase, decrease btns with object

--- redux-toolkit-tutorial-main/starter/src/features/cart/cartSlice.js
+++ redux-toolkit-tutorial-main/starter/src/features/cart/cartSlice.js
@@ -15,10 +15,22 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItem = [];
     },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItem = state.cartItem.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const cartItems = state.cartItem.find((item) => item.id === payload.id);
+      cartItems.amount = cartItems.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const cartItems = state.cartItem.find((item) => item.id === payload.id);
+      cartItems.amount = cartItems.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
