2d8d1c8b5ab71c406ded10b97909dfe97e47b435
Piotr <piotrdusinski01@gmail.com>
2024-01-14 20:56:14 +0100

    async from api with redux

--- redux-toolkit-tutorial-main/starter/src/App.js
+++ redux-toolkit-tutorial-main/starter/src/App.js
@@ -1,18 +1,30 @@
 import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import { useDispatch, useSelector } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import { useEffect } from 'react';
 import Modal from './components/Modal';
 
 function App() {
-  const { cartItem } = useSelector((store) => store.cart);
+  const { cartItem, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItem]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
 
   return (
     <main>
